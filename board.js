6a7c33c4cac9c375474d9bdc073b9e116d025b21
XanderReynolds <xane514@gmail.com>
2021-03-09 08:05:49 -0500

    delted commented code

--- board.js
+++ board.js
@@ -48,11 +48,7 @@ class Board {
         isHit = true;
       }
     }
-
-    for(const ship of this.shipList)
-    {
-        //console.log(`${ship.shipName} has ${ship.hits.length} of ${ship.positionArray.length} hits`)
-    }
+  
     return isHit;
   }
 
@@ -85,77 +81,6 @@ class Board {
       }  
     }
   }
-
-  // getStartCoordinates(direction, shipLength){
-  //   if(direction === 'horizontal')
-  //   {
-  //     return [this.getRandomInteger(0, this.numRows - shipLength), this.getRandomInteger(0, this.numCols)];
-  //   }
-  //   else {
-  //     return [this.getRandomInteger(0, this.numRows), this.getRandomInteger(0, this.numCols - shipLength)];
-  //   }
-  // }
-
-  // getRandomInteger(min, max) {
-  //   return Math.floor(Math.random() * (max - min) ) + min;
-  // }
-
-  // generateRandomDirection() {  
-  //   let x = (Math.floor(Math.random() * 2) == 0);
-  //   if(x){
-  //       return 'horizontal';
-  //   }else{
-  //       return 'vertical';
-  //   }
-  // }
-
-  // generateCoordinates(posX, posY, shipLength, direction) //startPosition is [x,y]
-  // {
-  //   let coordinates = [];
-    
-  //   if(direction == 'horizontal') {                       //horizontal, increment x
-  //     for(let i = posX; i < posX + shipLength; i++)
-  //     {
-  //       coordinates.push([i, posY]);
-  //     }
-  //   }else {                                               //vertical, increment y
-  //     for(let i = posY; i < posY + shipLength; i++)
-  //     {
-  //       coordinates.push([posX, i]);
-  //     }
-  //   }
-    
-  //   return coordinates;
-  //}
-
-  generateListOfRandomMoves(){
-    return BoardShipPlacerHelper.generateListOfRandomMoves(this.numRows, this.numCols);
-  }
-  //   const randomMoves = [];
-
-  //   Array.prototype.swap = function (x,y) {
-  //     var b = this[x];
-  //     this[x] = this[y];
-  //     this[y] = b;
-  //     return this;
-  //   }
-
-  //   //Create ordered list 
-  //   for(let i = 0; i < this.numRows; i++){
-  //     for(let j = 0; j < this.numCols; j++){
-  //       randomMoves.push([i,j]);
-  //     }
-  //   }
-
-  //   //Shuffle the order.
-  //   let totalNumbers = this.numRows * this.numCols;
-  //   for(let r = 0; r < (totalNumbers); r++){
-  //     let tempIndex = this.getRandomInteger(0, totalNumbers);
-  //     randomMoves.swap(r, tempIndex);      
-  //   }
-
-  //   return randomMoves;
-  // }
   
   addMove(move){
     this.moveList.push(move);
