34a2555cc5a386e27e0c3928b6eb535727442f04
XanderReynolds <xane514@gmail.com>
2021-03-09 07:31:39 -0500

    moved the logic into a class

---
+++ battleShip.js
@@ -0,0 +1,86 @@
+const Ship = require('./ship.js');
+const Move = require('./move.js');
+const HumanPlayer = require('./humanPlayer.js');
+const ComputerPlayer = require('./computerPlayer.js');
+const Board = require('./board.js');
+const ShipBuilder = require('./shipBuilder.js');
+const PlayerBuilder = require('./playerBuilder.js');
+const readlineSync = require('readline-sync');
+
+class BattleShip {
+
+  constructor(){}
+
+  playGame(playerList) {
+    
+    const boardPrototype = new Board(10, 10);
+    const gameShipList = new ShipBuilder().buildGameShips();
+    const gameCommands = ['Q', 'q', 'Y', 'y', 'N', 'n'];
+    const battleShipPlayerList = new PlayerBuilder().buildPlayerList(playerList, boardPrototype, gameShipList, gameCommands);
+
+    let input = readlineSync.question('Welcome to battleShip, press \'y\' to start: ');    
+
+    if(input == 'y' || input == 'Y')
+    {
+      let currentPlayer = battleShipPlayerList[0];
+      let nextPlayer = battleShipPlayerList[0];
+      let gameOver = false;
+
+      while(!gameOver)
+      {
+        currentPlayer = nextPlayer;
+        nextPlayer = this.getNextPlayer(currentPlayer, battleShipPlayerList);
+
+        if(nextPlayer == undefined) { console.error('Could not find next player'); break;}
+        let attackPos = currentPlayer.takeTurn();
+        
+        if(gameCommands.includes(attackPos)) { break;}
+
+        let isHit = nextPlayer.fireShots(attackPos);
+        let gameMove = new Move(attackPos[0], attackPos[1], isHit);   
+
+        currentPlayer.movePlayed(gameMove);
+       
+        console.log(`${currentPlayer.playerName} attacked position (${gameMove.X}, ${gameMove.Y}) of ${nextPlayer.playerName}. It was a ${isHit ? 'Hit' : 'Miss'}`);        
+        
+
+        gameOver = nextPlayer.areAllShipsSunk();
+
+        if(gameOver) { 
+          console.log(`${currentPlayer.playerName} is the Winner!`);
+          currentPlayer.board.printBoard();
+        }
+        
+      }
+    }
+
+    console.log('Thank you for playing today :)');
+  }
+  
+
+  getNextPlayer(currentPlayer, playerList)
+  {
+    let currentIndex = playerList.findIndex((player) => {return player === currentPlayer});
+
+    if(currentIndex === playerList.length-1)
+    {
+      const firstPlayer = playerList[0];
+      return firstPlayer;
+    }
+    else{
+      const nextPlayer = playerList[currentIndex+1];
+      return nextPlayer;
+    }
+
+    return undefined;  
+  }
+}
+
+module.exports = BattleShip;
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,75 +1,40 @@
-const Ship = require('./ship.js');
-const Move = require('./move.js');
-const HumanPlayer = require('./humanPlayer.js');
-const ComputerPlayer = require('./computerPlayer.js');
-const Board = require('./board.js');
-const ShipBuilder = require('./shipBuilder.js');
+const BattleShip = require('./battleShip.js');
 
-const readlineSync = require('readline-sync');
 
-const boardPrototype = new Board(10, 10);
-const gameShipList = new ShipBuilder().buildGameShips();
-const gameCommands = ['Q', 'q', 'Y', 'y', 'N', 'n'];
+const battleShip = new BattleShip();
 
-const player1 = new ComputerPlayer('Player1', boardPrototype, gameShipList, boardPrototype.generateListOfRandomMoves(), gameCommands);
-const player2 = new ComputerPlayer('Player2', boardPrototype, gameShipList, boardPrototype.generateListOfRandomMoves(), gameCommands);
-const player3 = new ComputerPlayer('Player3', boardPrototype, gameShipList, boardPrototype.generateListOfRandomMoves(), gameCommands);
-const player4 = new ComputerPlayer('Player4', boardPrototype, gameShipList, boardPrototype.generateListOfRandomMoves(), gameCommands);
-
-const playerList = [player1, player2, player3, player4];
-
-let input = readlineSync.question('Welcome to battleShip, press \'y\' to start: ');
-let currentPlayer = playerList[0];
-let nextPlayer = playerList[0];
-
-if(input == 'y' || input == 'Y')
-{
-  let gameOver = false; //
-  while(!gameOver)
+let playerList = [
   {
-    currentPlayer = nextPlayer;
-    nextPlayer = getNextPlayer(currentPlayer, playerList);
-
-    if(nextPlayer == undefined) { console.error('Could not find next player'); break;}
-    let attackPos = currentPlayer.takeTurn();
-
-    if(gameCommands.includes(attackPos)) { break;}
-
-    let isHit = nextPlayer.fireShots(attackPos);
-    let gameMove = new Move(attackPos[0], attackPos[1], isHit);   
-
-    currentPlayer.movePlayed(gameMove);
-    if(isHit){
-    console.log(`${currentPlayer.playerName} attacked position (${gameMove.X}, ${gameMove.Y}) of ${nextPlayer.playerName}. It was a ${isHit ? 'Hit' : 'Miss'}`);
-    }
-    gameOver = nextPlayer.areAllShipsSunk();
-
-    if(gameOver) { console.log(`${currentPlayer.playerName} is the Winner!`)}
-
-    //Is winner?
-  }
-}
-
-function getNextPlayer(currentPlayer, playerList)
-{
-  let currentIndex = playerList.findIndex((player) => {return player === currentPlayer});
-
-  if(currentIndex === playerList.length-1)
+    isHuman: true,
+    playerName: 'Player 1'
+  }, 
   {
-    const firstPlayer = playerList[0];
-    return firstPlayer;
+    isHuman: false,
+    playerName: 'Player 2'
   }
-  else{
-    const nextPlayer = playerList[currentIndex+1];
-    return nextPlayer;
+]
+
+battleShip.playGame(playerList);
+
+
+
+playerList = [
+  {
+    isHuman: false,
+    playerName: 'Han Solo'
+  }, 
+  {
+    isHuman: false,
+    playerName: 'Yoda'
+  },
+  {
+    isHuman: false,
+    playerName: 'Darth Vader'
+  },
+  {
+    isHuman: false,
+    playerName: 'Darth Maul'
   }
+]
 
-  return undefined;  
-}
-
-
-
-
-
-
-
+battleShip.playGame(playerList);
